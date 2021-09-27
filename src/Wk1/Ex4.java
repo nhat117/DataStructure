ccd65f8aede43d5e568b51d0cc90bcf0df93d765
Tommy <s3878174@rmit.edu.vn>
2021-09-27 20:30:37 +0700

    Add additional Excercise

--- src/Wk1/Ex4.java
+++ src/Wk1/Ex4.java
@@ -1,5 +1,13 @@
 package Wk1;
+/*
+Consider the following operations on an arbitrary positive integer, N
 
+If the N is even, divide it by two (i.e. N/2) and if N is odd, triple it and add one (i.e. 3*N +1)
+Now form a sequence by performing this operation repeatedly, beginning with any positive integer, and taking the result at each step as the input to the next and continue until the number 1 is reached.
+For instance, starting with n = 12, one gets the sequence 12, 6, 3, 10, 5, 16, 8, 4, 2, 1.  When n = 19, the sequence is much longer and is as follows: 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1.
+
+Write a program that for all the numbers between 1 and 100 calculates the number of steps it takes to reach 1 and print out the number of steps in a table
+ */
 public class Ex4 {
     //Check the step of each number to 1
     public static int stepcalc(int in) {
